66b8cf23c4039da2cbbf5193f6fdf705c987007f
rajukrpal <rajukumarpal95@gmail.com>
2024-08-08 09:41:01 +0530

    Add text

--- src/components/Form_C.jsx
+++ src/components/Form_C.jsx
@@ -5,18 +5,34 @@ import data from "../data";
 import { useState } from "react";
 
 const Form_C = () => {
-  const [url, setUrl] = useState("");
+  // const [url, setUrl] = useState("http://i.imgflip.com/1bij.jpg");
+  const [memoForm, setMemoForm] = useState({
+    firstName:"",
+    lastName:"",
+    url:"http://i.imgflip.com/1bij.jpg"
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const memesArray = data.data.memes;
     const renderNumber = Math.floor(Math.random() * memesArray.length);
     // const url = memesArray[renderNumber].url
-    const url = memesArray[renderNumber].url;
+    const ImgUrl = memesArray[renderNumber].url;
 
-    setUrl(url);
-    // console.log(url)
+    // setUrl(url);
+    setMemoForm(pre=>({
+      ...pre,
+      url:ImgUrl
+    }))
   };
+
+  const hancleChange = (e)=>{
+    const {name,value} = e.target
+    setMemoForm(pre=>({
+      ...pre,
+      [name] : value
+    }))
+  }
 
   return (
     <div className="px-5 py-2">
@@ -24,11 +40,21 @@ const Form_C = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="6">
             <Form.Label>Top text</Form.Label>
-            <Form.Control type="text" placeholder="Shut up" />
+            <Form.Control 
+            type="text" 
+            placeholder="Shut up"
+             name="firstName" 
+             value={memoForm.firstName} 
+             onChange={hancleChange}/>
           </Form.Group>
           <Form.Group as={Col} md="6">
             <Form.Label>Bottom text</Form.Label>
-            <Form.Control type="text" placeholder="And take my money" />
+            <Form.Control 
+            type="text" 
+            placeholder="And take my money"
+            name="lastName" 
+             value={memoForm.lastName}
+             onChange={hancleChange} />
           </Form.Group>
 
           <div className="py-3">
@@ -41,8 +67,10 @@ const Form_C = () => {
           </div>
         </Row>
       </Form>
-      <div className="flex justify-center">
-        <img className="md:h-96 h-40 md:w-auto " src={url} alt="" />
+      <div className="flex justify-center relative">
+        <img className="md:h-96 h-40 md:w-auto " src={memoForm.url} alt="" />
+        <h1 className="absolute top-0 text-white">{memoForm.firstName}</h1>
+        <h1 className="absolute bottom-0 text-white">{memoForm.lastName}</h1>
       </div>
     </div>
   );
